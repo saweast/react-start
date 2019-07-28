2b8644ae46e9286d8c5b4308b844ba2ff49d252d
Vadym Lavorchuk <lavorchukvadim@gmail.com>
2019-07-28 12:10:41 +0300

    React Tutorial For Beginners
    https://www.youtube.com/watch?v=dGcsHMXbSOA&list=PLDyQo7g0_nsVHmyZZpVJyFn5ojlboVEhE&index=2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Tweet from './Tweet';
+import './App.css'
+
 
 function App() {
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  }
+
+  const [users, setUsers] = useState([
+    { name: 'test', message: 'test message',},
+    {
+      name: 'test1', message: 'test message1',
+    },
+    {
+      name: 'test2', message: 'test message2',
+    },
+    {
+      name: 'test3', message: 'test message3',
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className={isRed ? 'red' : ''}>Change my color!</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+      <div>
+        {users.map(user => (
+          <Tweet name={user.name} message={user.message}/>
+        ))}
+      </div>
+      
     </div>
   );
 }
